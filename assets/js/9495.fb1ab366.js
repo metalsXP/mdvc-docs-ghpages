77ad301410022b2c77e28d4c54f1715b3f6285be
mzinnoecker <29286643+mzinnoecker@users.noreply.github.com>
2025-10-17 08:30:04 +0200

    redirection to metalsxp.services

--- assets/js/9495.fb1ab366.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkubc_services=self.webpackChunkubc_services||[]).push([[9495],{9495:(e,s,c)=>{c.d(s,{createPieServices:()=>r.f});var r=c(9732);c(3579)}}]);
--- assets/js/9669.e2a4fa83.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkubc_services=self.webpackChunkubc_services||[]).push([[9669],{9669:(e,s,c)=>{c.d(s,{createGitGraphServices:()=>r.b});var r=c(7123);c(3579)}}]);
